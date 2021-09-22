80bef57a0f13198007e7715b99906f1468289e28
Elisabeth Medlien <elisabethmedlien@gmail.com>
2021-09-22 04:36:12 +0200

    all functionality in place

--- README.md
+++ README.md
@@ -1,27 +1,16 @@
-# QuickorderTechnicalTask
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.2.6.
+# my decisions
+- I decided that the first row of the uploaded file will always be the table header
 
-## Development server
+# my issues
+- new to angular
+- fairly new to typescript
+(these issues were most time consuming for me)
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+# what I would want to fix if I had more time
+- I am using the any type. I would with more time and research replace them by actual types. Either custom defined type objects or already existing types
+- The file structure. 
 
-## Code scaffolding
-
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+# TODO
+- clean up code (any types ++)
+- structure code better. **COMPONENTS
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,76 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import * as XLSX from 'xlsx'; 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'quickorder-technical-task';
+
+  public records: any[] = [];
+  public sheets: any[] = [];
+  public workbook: any = [];
+
+  uploadFile($event: any): void {
+
+    const files = $event.srcElement.files;
+    const input = $event.target;
+    const reader = new FileReader();
+    
+    if (this.isCSVFile(files[0])){
+
+      reader.readAsText(input.files[0]);
+      reader.onload = () => {
+        const data: string | ArrayBuffer | null = reader.result;
+        this.records = this.csvFileToJSON(<string> data);
+      };
+    }
+
+    if (this.isXLSXFile(files[0])) {
+
+      reader.readAsBinaryString(files[0]);
+      reader.onload = () => {
+        const data = reader.result;
+        const workbook = XLSX.read(data, {type: 'binary'});
+        this.workbook = workbook;
+        const sheet = workbook.SheetNames;
+        this.sheets = sheet;
+        this.getSheet(workbook.SheetNames[0])
+      }
+    }
+  } 
+
+  getSheet(sheetName:string) {
+    const worksheet = this.workbook.Sheets[sheetName];
+    this.records = XLSX.utils.sheet_to_json(worksheet,{raw:true});
+  }
+
+  getKeys(jsonObject:any) {
+    return (jsonObject && jsonObject.length > 0) ? Object.keys(jsonObject[0]) : [];
+  }
+
+  csvFileToJSON(file:string){
+
+    const lines = file.split("\n");
+    const headers = <any>lines[0].split(",");
+
+    return new Array(lines.map((l:string) => {
+      const obj:Array<string> = [];
+      const currentline = l.split(",");
+
+      headers.map((e:string, i:number) => {
+        obj[headers[i]] = currentline[i]; // did not have time to consider a more type friendly way of handling this case
+      })
+    }))
+  }
+
+  isCSVFile(file: File) {
+    return file.name.endsWith(".csv");
+  }
+
+  isXLSXFile(file: File) {
+    return file.name.endsWith(".xlsx");
+  }
+
 }
